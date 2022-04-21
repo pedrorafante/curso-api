870e16f83daea420e9bfb82a4c4e55dd1387c114
Pedro Rafante <pedrorafante@gmail.com>
2022-04-21 10:16:44 -0300

    Perfil de testes

---
+++ src/main/java/br/com/pedrorafante/api/config/LocalConfig.java
@@ -0,0 +1,26 @@
+package br.com.pedrorafante.api.config;
+
+import br.com.pedrorafante.api.domain.User;
+import br.com.pedrorafante.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+@Configuration
+@Profile("local")
+public class LocalConfig {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Bean
+    public void startDB(){
+        User u1 = new User(null, "Rafante", "pedro@rafante.com", "123");
+        User u2 = new User(null, "Sanfona", "sanfona@rafante.com", "123");
+
+        repository.saveAll(List.of(u1,u2));
+    }
+}
