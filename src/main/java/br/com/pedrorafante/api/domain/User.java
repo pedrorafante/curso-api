65b8ae9deb02675028e627f718393a48641d097c
Pedro Rafante <pedrorafante@gmail.com>
2022-04-21 09:36:41 -0300

    Classe usuário

---
+++ src/main/java/br/com/pedrorafante/api/domain/User.java
@@ -0,0 +1,22 @@
+package br.com.pedrorafante.api.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
