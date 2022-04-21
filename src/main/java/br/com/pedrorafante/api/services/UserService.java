13416ab20e7bcfd0ad5dabd6b21aeb2b702750b6
Pedro Rafante <pedrorafante@gmail.com>
2022-04-21 12:49:02 -0300

    EndPoint create para user

--- src/main/java/br/com/pedrorafante/api/services/UserService.java
+++ src/main/java/br/com/pedrorafante/api/services/UserService.java
@@ -1,6 +1,7 @@
 package br.com.pedrorafante.api.services;
 
 import br.com.pedrorafante.api.domain.User;
+import br.com.pedrorafante.api.domain.dto.UserDTO;
 
 import java.util.List;
 
@@ -8,5 +9,6 @@ public interface UserService {
 
     User findById(Integer id);
     List<User> findAll();
+    User create(UserDTO obj);
 
 }
